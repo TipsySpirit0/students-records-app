7ce344d64cac59cd4813a4312fee0df80ea6191f
Tobiloba <tobitobe66@gmail.com>
2024-06-16 23:03:53 +0100

    feat: implement auth guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,22 @@
 import { Routes } from '@angular/router';
 import { LandingComponent } from './landing/landing.component';
 import { StudentRecordsComponent } from './student-records/student-records.component';
+import { LoginComponent } from './auth/login/login.component';
+import { authGuardGuard } from './auth-guard.guard';
 
 export const routes: Routes = [
-    {
-        path:"landing",
-        component: LandingComponent
-    },
-    {
-        path:"students",
-        component: StudentRecordsComponent
-    }
+  {
+    path: '',
+    component: LoginComponent,
+  },
+  {
+    path: 'landing',
+    component: LandingComponent,
+    canMatch: [authGuardGuard],
+  },
+  {
+    path: 'students',
+    component: StudentRecordsComponent,
+    canMatch: [authGuardGuard],
+  },
 ];
